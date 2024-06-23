478806521536ffa5b4dd88b3c4c82e223ac7a1d4
KatSjolin <kat.sjolin@gmail.com>
2024-06-24 00:52:57 +0200

    added animated arrow

--- my-portfolio/src/Components/Intro.jsx
+++ my-portfolio/src/Components/Intro.jsx
@@ -1,17 +1,20 @@
 import profilePic from '../assets/profile.jpg';
+import arrow from '../assets/arrow.svg';
 import "./Intro.css";
 
 const Intro = () => {
     return (
       <section id="intro">
-        <img src={profilePic} alt="Katarina Sjölin" />
+        <div className="profile-pic">
+          <img src={profilePic} alt="Katarina Sjölin" />
+        </div>
         <div className="text-container">
           <h1>Hi, I'm Katarina Sjölin</h1>
           <h2>Front end developer</h2>
           <p>I love programming and enjoy creating beautiful and functional web applications. I enjoy learning new skills and focus on user-friendliness.</p>
         </div>
         <div className="arrow">
-          <img src="/arrow.svg"alt="Arrow pointing down." />
+          <img src={arrow} alt="Arrow pointing down." />
         </div>
       </section>
     );
